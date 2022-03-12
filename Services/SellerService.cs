0d610fce59821901a9b8bef62deeffe8fa6af51e
Edigar Herculano <edigar.herculano@gruposbf.com.br>
2022-03-12 17:35:02 -0300

    feat: Add update seller and custom servive exception

--- Services/SellerService.cs
+++ Services/SellerService.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using WebSales.Data;
 using WebSales.Models;
 using Microsoft.EntityFrameworkCore;
+using WebSales.Services.Exceptions;
 
 namespace WebSales.Services
 {
@@ -38,6 +39,20 @@ namespace WebSales.Services
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            if (!_context.Seller.Any(x => x.Id == seller.Id)) throw new NotFoundException("Id not found");
+
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            } catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
